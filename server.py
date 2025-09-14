5397372ee8e5157b05727cf03479146d8e60b251
Vladan <vladandjurdjevic17@gmail.com>
2025-09-14 22:45:35 +0200

    Add CORS support

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # ✅ dozvoljava zahtjeve sa drugih domena
 
 @app.route('/submit-form', methods=['POST'])
 def receive_form():
-    data = request.json  # Ako šalješ JSON sa formulara
+    data = request.json
     print("Primljeni podaci:", data)
     return jsonify({"status": "success", "received": data})
 
